e95d108aadf1ccb2b309c601783d295ff6f7103b
xaxaxa1000 <77469329+xaxaxa1000@users.noreply.github.com>
2021-01-21 15:12:09 +0300

    Add files via upload

---
+++ 123.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <ctime>
+using namespace std;
+
+int main() {
+    
+    setlocale(LC_ALL, "Russian");
+    string ar[5]{ "Bip","Bup","Pup","Buup","Biip" };
+    char z;
+    double a,c;
+    c = 4;
+    double c1 = 1;
+    srand(time(0));
+        while (true) {
+            z = _getch();
+            if (z == '=') {
+                cout << "tone mult(standart 4):";
+                cin >> c;
+                cout << endl;
+                cout << "time mult(standart 1):";
+                cin >> c1;
+            }
+            a = static_cast<int>(z);
+            if (a < 0) {
+                a += 100;
+            }
+            //cout << z<<" "<<a << endl;
+            cout << ar[rand() % 5] << endl;
+            Beep(a*c, 500*c1);
+        }
+        
+}
